1ab9c39c9a91fea6c72b4c8ad5ecdb727ffa1361
KillianG <killian.gardahaut@epitech.eu>
2019-11-05 22:18:10 +0200

    Starts of some routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(plugin)]
+#![feature(concat_idents)]
+
+#[macro_use] extern crate rocket;
+
+use rocket::Rocket;
+
+/* -------------------- MODULES USAGES -------------------- */
+mod user;
+
+fn rocket() -> Rocket {
+    rocket::ignite()
+        .mount("/", routes![api_root])
+        .mount("/user", routes![user::login::login])
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket().launch();
+}
+
+#[get("/")]
+fn api_root() -> &'static str {
+    "Welcome to GeoWorker API (tips: you shouldn't be here)"
+}
+
+/* -------------------- UNIT TESTS -------------------- */
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn test_root() {
+        let client = Client::new(rocket()).expect("valid src instance");
+        let mut response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some("Welcome to GeoWorker API (tips: you shouldn't be here)".into()));
+    }
 }
---
+++ src/user/login.rs
@@ -0,0 +1,21 @@
+#[get("/login")]
+pub fn login() -> &'static str {
+    "Welcome to user login page"
+}
+
+/* -------------------- UNIT TESTS -------------------- */
+
+#[cfg(test)]
+mod test {
+    use super::super::super::rocket;
+    use rocket::local::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn test_root() {
+        let client = Client::new(rocket()).expect("valid src instance");
+        let mut response = client.get("/user/login").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some("Welcome to user login page".into()));
+    }
+}
